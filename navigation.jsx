3bdcb611a9024c141c3c3a95136946924f0393e5
Courtney Jackson <courtneymjackson8@gmail.com>
2026-04-23 16:55:01 -0400

    Login System

--- navigation.jsx
+++ navigation.jsx
@@ -1,7 +1,6 @@
-import TempScreen from "./screens/temp";
 import LoginScreen from "./screens/Login";
 import MessageScreen from "./screens/MessageScreen";
-import HomeScreen from "./screens/home";
+import SignUpScreen from "./screens/SignUp";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -13,16 +12,6 @@ export const NavigationStack = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "YOUSM Home" }}
-        />
-        <Stack.Screen
-          name="Temp"
-          component={TempScreen}
-          options={{ title: "Temp Screen" }}
-        />
-        <Stack.Screen
           name="Login"
           component={LoginScreen}
           options={{ headerShown: false }}
@@ -30,6 +19,11 @@ export const NavigationStack = () => {
         <Stack.Screen
           name="Message"
           component={MessageScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
           options={{ headerShown: false }}
         />
         {/* Copy the format of the stack.screen item above to add a new screen */}
